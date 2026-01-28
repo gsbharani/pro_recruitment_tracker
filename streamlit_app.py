9049957f6003f8969766a6f5a1ff74f531c5073c
gsbharani <gsbharani45@gmail.com>
2026-01-28 16:03:33 +0530

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.set_page_config(page_title="Recruitment Portal", layout="wide")
+
+st.title("Recruitment Management Portal")
+
+st.write("Welcome to the Recruitment Portal. Please use the sidebar to navigate.")
