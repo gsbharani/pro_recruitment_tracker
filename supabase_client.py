1980153f3e847c38b29676b4d31f7fc219ef5470
gsbharani <gsbharani45@gmail.com>
2026-01-28 16:18:34 +0530

    Update supabase_client.py

--- supabase_client.py
+++ supabase_client.py
@@ -1,12 +1,20 @@
-from supabase import create_client
+# supabase_client.py
 import os
+import psycopg2
+from psycopg2.extras import RealDictCursor
 from dotenv import load_dotenv
-
 
 load_dotenv()
 
+DB_URL = os.getenv("NEON_DB_URL")
 
-supabase = create_client(
-os.getenv("SUPABASE_URL"),
-os.getenv("SUPABASE_SERVICE_KEY")
-)
+conn = psycopg2.connect(DB_URL, cursor_factory=RealDictCursor)
+cursor = conn.cursor()
+
+def execute_query(query, params=None):
+    cursor.execute(query, params)
+    if query.strip().lower().startswith("select"):
+        return cursor.fetchall()
+    else:
+        conn.commit()
+        return cursor.rowcount
