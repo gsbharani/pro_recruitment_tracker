e607bb3f4cca49f7d2c38d1b152bdaa187c3d6bf
gsbharani <gsbharani45@gmail.com>
2026-01-28 14:56:30 +0530

    Create database.py

---
+++ app/database.py
@@ -0,0 +1,10 @@
+# app/database.py
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+DATABASE_URL = "postgresql://user:password@localhost/recruitment_db"
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(bind=engine)
+
+Base = declarative_base()
